cd60cb3b32055b4e5c9a1af439ce784aa2a5d657
Fabian Concha Sifuentes <80995242+Fabian2727@users.noreply.github.com>
2024-03-20 17:20:58 -0500

    Update Lab01.cpp

--- Lab01.cpp
+++ Lab01.cpp
@@ -1,48 +1,71 @@
-#include <iostream>    
-#include <chrono>   
+#include <iostream>
+#include <chrono>
 #include <ctime>
 using namespace std;
 
 int main() {
+	
+	const int max = 10000;
+	int** A = new int*[max];
+	int* x = new int[max];
+	int* y = new int[max]();
+	
+	for (int i = 0; i < max; ++i) {
+		A[i] = new int[max];
+	}
+	
+	auto start = std::chrono::system_clock::now();
+	
+	for (int i = 0; i < max; i++) {
+		for (int j = 0; j < max; j++) {
+			y[i] += A[i][j] * x[j];
+		}
+	}
+	
+	auto end = std::chrono::system_clock::now();
+	
+	std::chrono::duration<float, std::milli> duration = end - start;
+	
+	std::cout << duration.count() << "s " << std::endl;
+	
+	for (int i = 0; i < max; ++i) {
+		delete[] A[i];
+	}
+	delete[] A;
+	
+	delete[] x;
+	delete[] y;
+	
+	const int max2 = 10000;
+	int** B = new int*[max2];
+	int* a = new int[max2];
+	int* b = new int[max2]();
+	
+	for (int i = 0; i < max2; ++i) {
+		B[i] = new int[max2];
+	}
+	
+	auto start2 = std::chrono::system_clock::now();
+	
+	for (int j = 0; j < max2; j++) {
+		for (int i = 0; i < max2; i++) {
+			b[i] += B[i][j] * a[j];
+		}
+	}
+	
+	auto end2 = std::chrono::system_clock::now();
+	
+	std::chrono::duration<float, std::milli> duration2 = end2 - start2;
+	
+	std::cout << duration2.count() << "s " << std::endl;
 
-  const int max = 100;
-  int A[max][max];
-  int x[max];
-  int y[max] = {0};
-  
-  auto start = std::chrono::system_clock::now();
-  
-  for (int i = 0; i<max; i++)
-    {
-      for (int j = 0; j < max; j++)
-        {
-          y[i] += A[i][j] * x[j];
-        }
-    }
-  auto end = std::chrono::system_clock::now();
-  
-  std::chrono::duration<float,std::milli> duration = end - start;
-
-  std::cout << duration.count() << "s " << std::endl;
-
-
-  const int max2 = 100;
-  int B[max2][max2];
-  int a[max2];
-  int b[max2] = {0};
-  
-  auto start2 = std::chrono::system_clock::now();
-
-  for (int j = 0; j<max2; j++)
-    {
-      for (int i = 0; i < max2; i++)
-        {
-          b[i] += B[i][j] * a[j];
-        }
-    }
-  auto end2 = std::chrono::system_clock::now();
-
-  std::chrono::duration<float,std::milli> duration2 = end2 - start2;
-
-  std::cout << duration2.count() << "s " << std::endl;
+	for (int i = 0; i < max2; ++i) {
+		delete[] B[i];
+	}
+	delete[] B;
+	
+	delete[] a;
+	delete[] b;
+	
+	return 0;
 }
